1b45f45c942ff0c0d160cdc22da0671fceacd117
Tony <tomcatbuzz@yahoo.com>
2019-06-06 02:41:56 -0400

    rebuild V8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'your key',
+    authDomain: 'angularfire-firebaseapp.com',
+    databaseURL: 'https://angularfire-firebaseio.com',
+    projectId: 'angularfire-',
+    storageBucket: 'angularfire-appspot.com',
+    messagingSenderId: 'numbers here'
+  },
+  googleMapsKey: 'maps api key',
+
+  contentful: {
+    spaceId: 'space id',
+    token: 'token'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'your key',
+    authDomain: 'angularfire-firebaseapp.com',
+    databaseURL: 'https://angularfire-firebaseio.com',
+    projectId: 'angularfire-',
+    storageBucket: 'angularfire-appspot.com',
+    messagingSenderId: 'numbers here'
+  },
+  googleMapsKey: 'maps api key',
+
+  contentful: {
+    spaceId: 'space id',
+    token: 'token'
+  }
 };
 
 /*
